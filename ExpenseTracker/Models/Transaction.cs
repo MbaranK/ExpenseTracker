2bd9f279ca6552b474b18f6fdee8dc8c06f7243f
Baran Kalakoğlu <barankalakoglu7@gmail.com>
2022-12-28 00:05:25 +0300

    Add project files.

---
+++ ExpenseTracker/Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using Castle.Components.DictionaryAdapter;
+using System.Reflection.Metadata.Ecma335;
+using System.ComponentModel.DataAnnotations;
+using KeyAttribute = System.ComponentModel.DataAnnotations.KeyAttribute;
+using Castle.MicroKernel.SubSystems.Conversion;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        //CategoryId Foreign key
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        //
+        public int Amount { get; set; }
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now; // Default value.
+    }
+}
